ffd9cd73fa12ebc0c8f4b93269ecce2e71f2b357
Dave Feucht <poetas@gmail.com>
2018-03-28 20:44:11 +0200

    Refactored Category component to follow a more logical flow when determining if it is showing all posts or a single post

--- src/Category.jsx
+++ src/Category.jsx
@@ -24,6 +24,7 @@ class Category extends React.Component {
     };
   }
 
+  //Function to get all posts for a category
   _getCategoryPost(categoryId) {
     let getCategoryPostURI = "http://" + this.props.site + "/wp-json/wp/v2/posts?categories=" + categoryId;
     axios.get(getCategoryPostURI)
@@ -33,16 +34,17 @@ class Category extends React.Component {
           this.setState({categoryPost});
         }
       }, error => {
-        const errorMsg = "Did not work: " + (error.response ? error.response : error);
+        const errorMsg = "Could not get posts for this category: " + (error.response ? error.response : error);
         this.setState({ errorMsg });
       });
   }
 
+  //Function to open a specific category when it is clicked
   _openCategory() {
-    this.setState({showAllPosts: !this.state.showAllPosts}); 
     this.props.clickCategory(this.props.id, this.props.name);
   }
 
+  //When the component is about to mount, get the list of posts for the category
   componentWillMount() {
     this._getCategoryPost(this.props.id);
   }
@@ -51,15 +53,22 @@ class Category extends React.Component {
     let postBody = "";
     let categoryTitle = <div className="category-title-clickable">{this.props.name}</div>;
     let categorySubtitle = "";
-    if(!this.props.showAllPosts) {
-      if(!(Object.keys(this.state.categoryPost).length === 0 && this.state.categoryPost.constructor === Object)) {
-        postBody = <Post key={this.state.categoryPost.id.toString()} category={this.props.id} categoryName={this.props.name} title={this.state.categoryPost.title.rendered} id={this.state.categoryPost.id} image={this.state.categoryPost.featured_media} context="category-image" site={this.props.site} />;
-      }
-    }
-    else {
+
+    //If we are displaying all posts, display the Posts component
+    if(this.props.showAllPosts) {
       categoryTitle = <div className="category-title">{this.props.name}</div>;
       categorySubtitle = <div className="category-subtitle">Back to Categories</div>; 
       postBody = <Posts site={this.props.site} category={this.props.id} />;
+      
+    }
+    
+    //Otherwise display the single post that was clicked
+    else {
+      //If posts could be retrieved, display the single post which was clicked
+      if(!(Object.keys(this.state.categoryPost).length === 0 && this.state.categoryPost.constructor === Object)) {
+
+        postBody = <Post key={this.state.categoryPost.id.toString()} category={this.props.id} categoryName={this.props.name} title={this.state.categoryPost.title.rendered} id={this.state.categoryPost.id} image={this.state.categoryPost.featured_media} context="category-image" site={this.props.site} />;
+      }
     }
     return ( 
       <div className="category" onClick={this._openCategory.bind(this)}>
@@ -74,6 +83,7 @@ class Category extends React.Component {
 
 }
 
+//Set up propTypes for type checking
 Category.propTypes = {
   id: PropTypes.number,
   name: PropTypes.string,
