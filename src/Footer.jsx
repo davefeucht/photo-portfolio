beb8ceede53c33724753c76e98e0b6c1566b4fed
Dave Feucht <poetas@gmail.com>
2018-06-14 22:40:59 +0200

    Splits footer into a separate component

---
+++ src/Footer.jsx
@@ -0,0 +1,17 @@
+/*****************
+* Footer component implements the footer of the application
+*****************/
+
+import React from "react";
+
+
+export default class Footer extends React.Component {
+
+  render () {
+    return (
+      <div className="site-footer">
+        Copyright @2018 Dave Feucht  
+      </div>
+    );
+  }
+}
