63fdb7a8b3020b3865e5589c5b4a3d394f87aa7c
Dave Feucht <poetas@gmail.com>
2018-12-19 17:59:36 +0100

    Initial basic jest setup

---
+++ src/test/titlebar.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import TitleBar from '../components/TitleBar';
+import renderer from 'react-test-renderer';
+
+test('Titlebar displays', () => {
+  const component = renderer.create(
+    <TitleBar></TitleBar>
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
