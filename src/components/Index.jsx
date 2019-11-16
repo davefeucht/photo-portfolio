7f49ff54f3c3968780474d2761ea9c1df9a5bddb
Dave Feucht <poetas@gmail.com>
2019-11-16 15:50:03 +0100

    Removes redux imports

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -4,16 +4,11 @@
 
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import { devToolsEnhancer } from 'redux-devtools-extension';
 import PhotoPortfolio from './PhotoPortfolio/PhotoPortfolio.jsx';
 import stateStore from '../store/state_store';
-import photoPortfolioState from '../reducers/reducers.js';
 import '../sass/main.scss';
 
 const mobxStore = new stateStore();
-const store = createStore(photoPortfolioState, /* preloadedState, */ devToolsEnhancer());
 
 //Render the PhotoPortfolio component in the 'photo-portfolio' container on the page.
 render(
