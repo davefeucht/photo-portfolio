2c35c7897e513f0e73b05900623bf8f20316975d
Dave Feucht <poetas@gmail.com>
2019-11-17 13:19:06 +0100

    Updates jest config to work again

--- src/test/titlebar.test.js
+++ src/test/titlebar.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import TitleBar from '../components/TitleBar';
+import TitleBar from '../components/TitleBar/TitleBar';
 import renderer from 'react-test-renderer';
 
 test('Titlebar displays', () => {
