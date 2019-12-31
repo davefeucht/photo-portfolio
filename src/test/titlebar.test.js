d62de1921c572fb511cc9a49a7daae041a3a0253
Dave Feucht <poetas@gmail.com>
2019-12-31 14:47:35 +0100

    Updates test to pass API to TitleBar

--- src/test/titlebar.test.js
+++ src/test/titlebar.test.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import renderer from 'react-test-renderer';
 import TitleBar from '../components/TitleBar/TitleBar';
 import stateStore from '../store/store';
-import renderer from 'react-test-renderer';
+import API from '../utils/Api';
 
 test('Titlebar displays', () => {
   const store = new stateStore();
+  const api = new API(store);
   const component = renderer.create(
-    <TitleBar stateStore={store}></TitleBar>
+    <TitleBar stateStore={store} api={api}></TitleBar>
   );
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
