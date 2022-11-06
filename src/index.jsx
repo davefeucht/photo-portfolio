18f2e13567be75c17014ffa7a1ee6129ea8c5ade
Dave Feucht <poetas@gmail.com>
2022-11-06 13:00:33 +0100

    Update to React 18

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@
 ****************/
 
 import React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { HashRouter as Router } from 'react-router-dom';
 import PhotoPortfolio from './components/PhotoPortfolio/PhotoPortfolio.jsx';
 import stateStore from './StateStore/store';
@@ -13,11 +13,11 @@ import './assets/stylesheets/main.scss';
 const store = new stateStore();
 const api = new API(store);
 store.setApplicationRoot(document.getElementById('photo-portfolio'));
+const root = createRoot(store.applicationRoot);
 
 //Render the PhotoPortfolio component in the 'photo-portfolio' container on the page.
-render(
+root.render(
   <Router>
     <PhotoPortfolio stateStore={store} api={api}/>
-  </Router>,
-  store.applicationRoot
+  </Router>
 );
