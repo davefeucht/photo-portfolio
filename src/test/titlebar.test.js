31e16dcc9df5e91fbefa0fb0f00443ab2b108886
Dave Feucht <poetas@gmail.com>
2022-11-27 13:22:39 +0100

    Update test for TitleBar to remove API test

--- src/test/titlebar.test.js
+++ src/test/titlebar.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import TitleBar from '../components/TitleBar/TitleBar';
 import stateStore from '../StateStore/store';
-import API from '../utils/Api';
 
 const siteInfo = {
     name: 'Through a Pinhole'
@@ -14,9 +13,8 @@ jest.mock("../utils/Api", () => ({
 
 test('Titlebar displays', () => {
   const store = new stateStore();
-  const api = new API(store);
   const component = renderer.create(
-    <TitleBar stateStore={store} api={api}></TitleBar>
+    <TitleBar stateStore={store}></TitleBar>
   );
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
