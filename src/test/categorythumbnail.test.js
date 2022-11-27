29b1b4b329b161594d6e1ef118fdbf8d9b96ee93
Dave Feucht <poetas@gmail.com>
2022-11-27 14:36:59 +0100

    Create basic test for CategoryThumbnail and remove snapshots from gitignore

--- src/test/categorythumbnail.test.js
+++ src/test/categorythumbnail.test.js
@@ -1,21 +1,27 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
-import TitleBar from '../components/CategoryThumbnail/CategoryThumbnail';
+import renderer, { act } from 'react-test-renderer';
+import { MemoryRouter } from 'react-router-dom';
+import CategoryThumbnail from '../components/CategoryThumbnail/CategoryThumbnail';
 import stateStore from '../StateStore/store';
 
-const siteInfo = {
-    name: 'Through a Pinhole'
-};
+const id = 5;
+const name = 'Test Category';
+const thumbnailImageUrl = "";
 
 jest.mock("../utils/Api", () => ({
-    getSiteInfo: jest.fn(() => Promise.resolve(siteInfo))
+    getCategoryImage: jest.fn(() => Promise.resolve(thumbnailImageUrl))
   }));
 
-test('Titlebar displays', () => {
+test('CategoryThumbnail displays', () => {
   const store = new stateStore();
   const component = renderer.create(
-    <TitleBar stateStore={store}></TitleBar>
+      <MemoryRouter>
+          <CategoryThumbnail id={id} name={name} stateStore={store}></CategoryThumbnail>
+      </MemoryRouter>
   );
-  let tree = component.toJSON();
+  let tree;
+  act(() => {
+    tree = component.toJSON();
+  });
   expect(tree).toMatchSnapshot();
 });
