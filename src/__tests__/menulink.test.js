bb0296433b12ff6355d33752dcc50758542caeaa
Dave Feucht <poetas@gmail.com>
2022-12-22 13:35:31 +0100

    Adds snapshot test for MenuLink

--- src/__tests__/menulink.test.js
+++ src/__tests__/menulink.test.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { MemoryRouter } from 'react-router-dom';
-import Menu from '../components/Menu/Menu';
-import stateStore from '../StateStore/store';
+import MenuLink from '../components/MenuLink/MenuLink';
 
-test('Menu displays', async () => {
-    const store = new stateStore();
+const href = '/page/150';
+const text = 'Contact'
+
+test('MenuLink displays', async () => {
     let component;
     let tree;
     component = renderer.create(
         <MemoryRouter>
-            <Menu stateStore={store}></Menu>
+            <MenuLink href={href} text={text}></MenuLink>
         </MemoryRouter>
     );
     tree = component.toJSON();
