a6bcc46061da6868b011db2407b7335146092796
Dave Feucht <poetas@gmail.com>
2022-12-22 13:44:01 +0100

    Adds snapshot test for Page

---
+++ src/__tests__/page.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { MemoryRouter } from 'react-router-dom';
+import Page from '../components/Page/Page';
+import stateStore from '../StateStore/store';
+
+const page = {
+    id: 150,
+    content: {
+        rendered: 'Contact me!'
+    },
+    title: {
+        rendered: 'Contact'
+    }
+};
+
+jest.mock('../utils/Api', () => ({
+    getPage: () => Promise.resolve(page)
+}));
+
+test('Category displays', () => {
+  const store = new stateStore();
+  let component;
+  let tree;
+  component = renderer.create(
+      <MemoryRouter initialEntries={['/page/150']} >
+          <Page stateStore={store}></Page>
+      </MemoryRouter>
+  );
+  tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
