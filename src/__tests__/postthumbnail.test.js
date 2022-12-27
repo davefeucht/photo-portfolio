e269b62a3428aa17a30c6d42f01ee45734f97ce1
David Feucht <poetas@gmail.com>
2022-12-27 22:24:23 +0100

    Adds snapshot tests for PostNavigationArrow and PostThumbnail

--- src/__tests__/postthumbnail.test.js
+++ src/__tests__/postthumbnail.test.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { MemoryRouter } from 'react-router-dom';
-import PostNavigationArrow from '../components/PostNavigationArrow/PostNavigationArrow';
+import PostThumbnail from '../components/PostThumbnail/PostThumbnail';
 import stateStore from '../StateStore/store';
 
-const direction = 'previous';
+const categoryPosts = [
+    {
+        thumbnail_image: 'some_url'
+    }
+];
+
 const postId = 35;
 
-test('PostNavigationArrow displays', () => {
+test('PostThumbnail displays', () => {
   const store = new stateStore();
   let component;
   let tree;
+  store.setCategoryPosts(categoryPosts);
   component = renderer.create(
       <MemoryRouter >
-          <PostNavigationArrow stateStore={store} direction={direction} postId={postId}></PostNavigationArrow>
+          <PostThumbnail stateStore={store} id={postId} index={0}></PostThumbnail>
       </MemoryRouter>
   );
   tree = component.toJSON();
