946fc5180542565f32e44c343cf44e0137a1bbbf
Dave Feucht <poetas@gmail.com>
2023-06-02 09:33:32 +0200

    Updates reference to babel config file

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
           test: /\.jsx?$/,
           include: [path.join(__dirname, 'src')],
           loader: 'babel-loader',
-          options: { configFile: path.resolve(__dirname, './.babelrc') }
+          options: { configFile: path.resolve(__dirname, './babel.config.json') }
       },
       { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'] },
       { test: /\.css$/, use: ['style-loader', 'css-loader'] },
