849b82e9ff79fee4fa6ef0d34c3e07168032f988
Dave Feucht <poetas@gmail.com>
2023-06-16 13:44:31 +0200

    Add full API mock to Post test

--- src/components/__tests__/post.test.jsx
+++ src/components/__tests__/post.test.jsx
@@ -5,20 +5,76 @@ import { MemoryRouter } from 'react-router-dom';
 import StateStore from '../../StateStore/store';
 import Post from '../Post/Post';
 
-const post = {
-    id: 887,
-    featured_media: 142,
-    categories: [220]
-};
+const pages = [
+    {
+        id: 150,
+        content: {
+            rendered: 'Contact me!'
+        },
+        title: {
+            rendered: 'Contact'
+        }
+    }
+];
 
-const tagNames = ['tag1', 'tag2', 'tag3'];
+const categories = [
+    {
+        id: 23,
+        count: 10,
+        description: 'Category of other things',
+        link: '',
+        name: 'Some other category',
+        slug: 'smocg',
+        taxonomy: '',
+        parent: 12,
+        meta: {}
+    },
+    {
+        id: 35,
+        count: 3,
+        description: 'Category of things',
+        link: '',
+        name: 'Some category',
+        slug: 'smcg',
+        taxonomy: '',
+        parent: 23,
+        meta: {}
+    }
+];
 
-const href = 'https://fooimage';
+const posts = [
+    {
+        id: 'foo',
+        link: 'http://foo',
+        modified: '12308213098',
+        slug: 'foo',
+        type: 'foo',
+        title: {
+            rendered: 'foo'
+        },
+        content: {
+            rendered: 'foo'
+        },
+        author: 0,
+        featured_media: 0,
+        categories: [0, 1],
+        tags: [0, 1],
+        thumbnail_image: 'http:/foo'
+    }
+];
 
 jest.mock('../../utils/Api', () => ({
-    getPost: () => Promise.resolve(post),
-    getTagNames: () => Promise.resolve(tagNames),
-    getPostImage: () => Promise.resolve(href)
+    getPostThumbnail: () => Promise.resolve('http://foo'),
+    getSiteInfo: () => Promise.resolve('Though a Pinhole'),
+    getCategories: () => Promise.resolve(categories),
+    getCategoryImage: () => Promise.resolve('http://foo'),
+    getPosts: () => Promise.resolve(posts),
+    getPost: () => Promise.resolve(posts[0]),
+    getCategoryInfo: () => Promise.resolve(),
+    getPostImage: () => Promise.resolve('http://foo'),
+    getTagNames: () => Promise.resolve(['foo', 'bob']),
+    getPages: () => Promise.resolve(pages),
+    getPage: () => Promise.resolve(pages[0])
 }));
 
 test('Post displays', () => {
