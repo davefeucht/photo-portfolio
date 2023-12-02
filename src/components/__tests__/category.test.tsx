ac1dca07c2cf3ccb54763748e7edd5070fbcbc24
Dave Feucht <poetas@gmail.com>
2023-12-02 19:24:36 +0100

    Setup lint-staged

---
+++ src/components/__tests__/category.test.tsx
@@ -0,0 +1,54 @@
+import { render, waitFor } from '@testing-library/react';
+import * as React from 'react';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
+
+import StateStore from '../../StateStore/store';
+import { ApiContext } from '../../utils/ApiContext';
+import WordpressAPI from '../../utils/WordpressAPI';
+import Category from '../Category/Category';
+
+const screenInfo = {
+    width: 500,
+    height: 500
+};
+
+jest.mock('../../utils/WordpressAPI');
+
+const setCategoryPostsMock = jest.fn();
+const setCategoryDataMock = jest.fn();
+const setThumbnailImageUrlMock = jest.fn();
+
+test('Category displays', async () => {
+    const store = new StateStore();
+    const api = new WordpressAPI(store.siteInfo.siteUrl);
+    const posts = await api.getPosts(35);
+    const category = await api.getCategoryInfo(35);
+    const { container } = render(
+        <MemoryRouter initialEntries={['/category/35']}>
+            <ApiContext.Provider value={api}>
+                <Routes>
+                    <Route
+                        path="category/:categoryId"
+                        element={(
+                            <Category
+                                maxItemsPerPage={2}
+                                screenInfo={screenInfo}
+                                currentCategoryPosts={posts}
+                                currentCategoryData={category}
+                                setCategoryPosts={setCategoryPostsMock}
+                                setCategoryData={setCategoryDataMock}
+                                setThumbnailImageUrl={setThumbnailImageUrlMock}
+                            />
+                        )}
+                    />
+                </Routes>
+            </ApiContext.Provider>
+        </MemoryRouter>
+    );
+    await waitFor(() => {
+        expect(setCategoryPostsMock).toHaveBeenCalledTimes(1);
+        expect(setCategoryDataMock).toHaveBeenCalledTimes(1);
+        expect(setThumbnailImageUrlMock).toHaveBeenCalledTimes(2);
+    });
+    expect(container.firstChild).toMatchSnapshot();
+});
